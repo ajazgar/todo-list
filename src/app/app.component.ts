f721d40f6432b7abe3fefd1165a8a95be076917c
ajazgar <asia.jazgar@gmail.com>
2018-09-19 21:13:18 +0200

    header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {TodoDataService} from '../todo/todo-data.service';
+import {Todo} from '../todo/todo';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [TodoDataService]
 })
 export class AppComponent {
-  title = 'app';
+
+  constructor(private todoDataService: TodoDataService) {
+  }
+
+  onAddTodo(todo: Todo) {
+    this.todoDataService.addTodo(todo);
+  }
+
+  toggleTodoComplete(todo) {
+    this.todoDataService.toggleTodoComplete(todo);
+  }
+
+  removeTodo(todo) {
+    this.todoDataService.deleteTodoById(todo.id);
+  }
+
+  get todos() {
+    return this.todoDataService.getAllTodos();
+  }
+
 }
